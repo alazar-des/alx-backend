e40ac95e29311c766bb7ef0229c3b4dd4bff0e45
alazar-des <alazar.des@gmail.com>
2021-12-21 16:49:42 +0300

    LRU cahche

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" LRU caching
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ Put and get to a caching dictionary
+    """
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """ put key value pair to dict
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                disc_key = next(iter(self.cache_data))
+                print("DISCARD: {}".format(disc_key))
+                del self.cache_data[disc_key]
+            else:
+                self.cache_data.pop(key)
+                self.cache_data[key] = item
+
+    def get(self, key):
+        """ return value of key
+        """
+        item = self.cache_data.get(key)
+        if item:
+            self.cache_data.pop(key)
+            self.cache_data[key] = item
+        return item
