122cca891b93e522ce3ec7abc9895fafdb785c14
alazar-des <alazar.des@gmail.com>
2022-02-02 13:38:57 +0300

    promisify function

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,34 @@
+import { createClient } from 'redis';
+
+import { promisify } from 'util';
+
+const redis = require('redis');
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server', err);
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+const setNewSchool = (schoolName, value) => {
+  client.set(schoolName, value, redis.print);
+};
+
+let displaySchoolValue = (schoolName) => {
+  client.get(schoolName, (err, value) => {
+    if (err) throw err;
+    console.log(value);
+  });
+};
+
+displaySchoolValue = promisify(displaySchoolValue);
+
+displaySchoolValue('Holberton')
+  .then(() => {})
+  .catch((err) => console.log(err));
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
